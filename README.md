ffffe8bc01c0e2fecabe52492373d447b99a02f5
Tyler Watson (He/Him) <107147939+tylerww91@users.noreply.github.com>
2022-09-12 14:14:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shell Game
+
+🦸 🦸‍♂️ `Stop starting and start finishing.` 🏁
+
+If you work on more than one feature at a time, you are guaranteed to multiply your bugs and your anxiety.
+
+## Developer Notes
---
+++ app.js
@@ -0,0 +1,17 @@
+/* Imports */
+// import { getRandomItem } from './utils.js';
+
+/* State */
+
+/* Actions */
+function loadPage() {}
+
+/* Components */
+
+/* Component */
+// get DOM
+// display
+// event listeners
+
+/* Run page load code */
+loadPage();
